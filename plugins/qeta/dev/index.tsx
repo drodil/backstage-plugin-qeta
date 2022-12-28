38c34f9cac64f0a9bbd2357463d83e241e9244f8
Heikki Hellgren <heikki.hellgren@op.fi>
2022-12-28 14:25:04 +0200

    fix: move catalog api to dev/index
    this is so that it will not be created when using qeta plugin as such.
    
    closes #16

--- plugins/qeta/dev/index.tsx
+++ plugins/qeta/dev/index.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
 import { createDevApp } from '@backstage/dev-utils';
-import { QetaPage, qetaPlugin } from '../src/plugin';
+import { QetaPage } from '../src/plugin';
+import {
+  AnyApiFactory,
+  createApiFactory,
+  createPlugin,
+  discoveryApiRef,
+  fetchApiRef,
+} from '@backstage/core-plugin-api';
+import { rootRouteRef } from '../src/routes';
+import { catalogApiRef } from '@backstage/plugin-catalog-react';
+import { CatalogClient } from '@backstage/catalog-client';
+
+const apiFactories: AnyApiFactory[] = [
+  createApiFactory({
+    api: catalogApiRef,
+    deps: { discoveryApi: discoveryApiRef, fetchApi: fetchApiRef },
+    factory: ({ discoveryApi, fetchApi }) =>
+      new CatalogClient({ discoveryApi, fetchApi }),
+  }),
+];
+
+const qetaDevPlugin = createPlugin({
+  id: 'qetaDev',
+  routes: {
+    root: rootRouteRef,
+  },
+  apis: apiFactories,
+});
 
 createDevApp()
-  .registerPlugin(qetaPlugin)
+  .registerPlugin(qetaDevPlugin)
   .addPage({
     element: <QetaPage />,
     title: 'Root Page',
--- plugins/qeta/src/plugin.ts
+++ plugins/qeta/src/plugin.ts
@@ -1,5 +1,4 @@
 import {
-  AnyApiFactory,
   configApiRef,
   createApiFactory,
   createPlugin,
@@ -9,36 +8,20 @@ import {
 
 import { rootRouteRef } from './routes';
 import { qetaApiRef, QetaClient } from './api';
-// import { catalogApiRef } from '@backstage/plugin-catalog-react';
-// import { CatalogClient } from '@backstage/catalog-client';
-
-const apiFactories: AnyApiFactory[] = [
-  createApiFactory({
-    api: qetaApiRef,
-    deps: { configApi: configApiRef, fetchApi: fetchApiRef },
-    factory: ({ configApi, fetchApi }) =>
-      new QetaClient({ configApi, fetchApi }),
-  }),
-];
-
-// TODO: Figure way to only add catalog api in local development
-// If this is enabled and used by a real backstage app, it will
-// die horribly with an error.
-/* const catalogApi = createApiFactory({
-  api: catalogApiRef,
-  deps: { discoveryApi: discoveryApiRef, fetchApi: fetchApiRef },
-  factory: ({ discoveryApi, fetchApi }) =>
-    new CatalogClient({ discoveryApi, fetchApi }),
-});
-apiFactories.push(catalogApi);
- */
 
 export const qetaPlugin = createPlugin({
   id: 'qeta',
   routes: {
     root: rootRouteRef,
   },
-  apis: apiFactories,
+  apis: [
+    createApiFactory({
+      api: qetaApiRef,
+      deps: { configApi: configApiRef, fetchApi: fetchApiRef },
+      factory: ({ configApi, fetchApi }) =>
+        new QetaClient({ configApi, fetchApi }),
+    }),
+  ],
 });
 
 export const QetaPage = qetaPlugin.provide(
