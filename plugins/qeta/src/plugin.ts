2ce629ba777f9ea98301e0f14ee82086ca747912
Heikki Hellgren <heikki.hellgren@op.fi>
2022-12-28 12:43:10 +0200

    fix: duplicate catalog api for plugin users

--- plugins/qeta/src/plugin.ts
+++ plugins/qeta/src/plugin.ts
@@ -1,4 +1,5 @@
 import {
+  AnyApiFactory,
   configApiRef,
   createApiFactory,
   createPlugin,
@@ -12,25 +13,33 @@ import { qetaApiRef, QetaClient } from './api';
 import { catalogApiRef } from '@backstage/plugin-catalog-react';
 import { CatalogClient } from '@backstage/catalog-client';
 
+const apiFactories: AnyApiFactory[] = [
+  createApiFactory({
+    api: qetaApiRef,
+    deps: { configApi: configApiRef, fetchApi: fetchApiRef },
+    factory: ({ configApi, fetchApi }) =>
+      new QetaClient({ configApi, fetchApi }),
+  }),
+];
+if (process.env.NODE_ENV === 'development') {
+  try {
+    const catalogApi = createApiFactory({
+      api: catalogApiRef,
+      deps: { discoveryApi: discoveryApiRef, fetchApi: fetchApiRef },
+      factory: ({ discoveryApi, fetchApi }) =>
+        new CatalogClient({ discoveryApi, fetchApi }),
+    });
+    apiFactories.push(catalogApi);
+  } catch (_) {
+    // NOOP
+  }
+}
 export const qetaPlugin = createPlugin({
   id: 'qeta',
   routes: {
     root: rootRouteRef,
   },
-  apis: [
-    createApiFactory({
-      api: qetaApiRef,
-      deps: { configApi: configApiRef, fetchApi: fetchApiRef },
-      factory: ({ configApi, fetchApi }) =>
-        new QetaClient({ configApi, fetchApi }),
-    }),
-    createApiFactory({
-      api: catalogApiRef,
-      deps: { discoveryApi: discoveryApiRef, fetchApi: fetchApiRef },
-      factory: ({ discoveryApi, fetchApi }) =>
-        new CatalogClient({ discoveryApi, fetchApi }),
-    }),
-  ],
+  apis: apiFactories,
 });
 
 export const QetaPage = qetaPlugin.provide(
