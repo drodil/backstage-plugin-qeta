384c378a5c43dec9f592477b76ba64d4fb9acd4d
Heikki Hellgren <heikki.hellgren@op.fi>
2023-01-02 11:50:58 +0200

    feat: remember filter panel options
    closes #15

--- plugins/qeta/src/components/QuestionsContainer/QuestionsContainer.tsx
+++ plugins/qeta/src/components/QuestionsContainer/QuestionsContainer.tsx
@@ -1,9 +1,10 @@
 import { useQetaApi } from '../../utils/hooks';
 import { Box, Button, Collapse, Grid, Typography } from '@material-ui/core';
-import React from 'react';
-import { FilterKey, FilterPanel } from './FilterPanel';
+import React, { useEffect } from 'react';
+import { FilterKey, filterKeys, FilterPanel } from './FilterPanel';
 import { QuestionList } from './QuestionList';
 import FilterList from '@material-ui/icons/FilterList';
+import { useSearchParams } from 'react-router-dom';
 
 export interface QuestionsContainerProps {
   tags?: string[];
@@ -15,22 +16,51 @@ export const QuestionsContainer = (props: QuestionsContainerProps) => {
   const { tags, author, entity, showFilters } = props;
   const [page, setPage] = React.useState(1);
   const [showFilterPanel, setShowFilterPanel] = React.useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
   const [filters, setFilters] = React.useState({
     order: 'desc',
     orderBy: 'created',
     noAnswers: 'false',
     noCorrectAnswer: 'false',
+    noVotes: 'false',
   });
   const pageSize = 10;
   const offset = (page - 1) * pageSize;
 
   const onPageChange = (value: number) => {
     setPage(value);
+    setSearchParams(prev => {
+      const newValue = prev;
+      newValue.set('page', String(value));
+      return newValue;
+    });
   };
 
   const onFilterChange = (key: FilterKey, value: string) => {
+    onPageChange(1);
     setFilters({ ...filters, ...{ [key]: value } });
+    setSearchParams(prev => {
+      const newValue = prev;
+      newValue.set(key, value);
+      return newValue;
+    });
   };
+
+  useEffect(() => {
+    let filtersApplied = false;
+    searchParams.forEach((value, key) => {
+      if (key === 'page') {
+        setPage(Number.parseInt(value, 10));
+      } else if (key in filterKeys) {
+        filtersApplied = true;
+        filters[key as FilterKey] = value;
+      }
+    });
+    setFilters(filters);
+    if (filtersApplied) {
+      setShowFilterPanel(true);
+    }
+  }, [searchParams, filters]);
 
   const {
     value: response,
@@ -80,6 +110,7 @@ export const QuestionsContainer = (props: QuestionsContainerProps) => {
         response={response}
         onPageChange={onPageChange}
         entity={entity}
+        page={page}
       />
     </Box>
   );
