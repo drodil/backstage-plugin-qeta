e79bf3f12701e18651cf58f47019e8bd62ac92cb
Heikki Hellgren <heikki.hellgren@op.fi>
2023-01-16 14:36:00 +0200

    feat: format all entities the same way

--- plugins/qeta/src/utils/utils.ts
+++ plugins/qeta/src/utils/utils.ts
@@ -1,9 +1,5 @@
 import { Entity, stringifyEntityRef } from '@backstage/catalog-model';
 
-export const getEntityTitle = (entity: Entity) => {
-  return entity.metadata.title ?? stringifyEntityRef(entity);
-};
-
 export const getEntityUrl = (entity: Entity) => {
   return `/catalog/${entity.metadata.namespace ?? 'default'}/${entity.kind}/${
     entity.metadata.name
@@ -16,4 +12,8 @@ export const formatEntityName = (username: string) => {
     ?.split(/[_.-]+/)
     .map(a => a.charAt(0).toUpperCase() + a.slice(1))
     .join(' ');
+};
+
+export const getEntityTitle = (entity: Entity) => {
+  return formatEntityName(entity.metadata.title ?? stringifyEntityRef(entity));
 };
