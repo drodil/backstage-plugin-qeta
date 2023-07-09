6547e6c5defff3e97bc6eb742511517bbf12b7a9
Ben Koski <bkoski@nytimes.com>
2023-07-09 12:10:48 -0400

    feat: use entity title on entity ask page

--- plugins/qeta/src/components/AskPage/AskPage.tsx
+++ plugins/qeta/src/components/AskPage/AskPage.tsx
@@ -4,15 +4,25 @@ import React from 'react';
 
 import { AskForm } from '../AskForm/AskForm';
 import { useParams } from 'react-router-dom';
+import { formatEntityName } from '../../utils/utils';
 import { BackToQuestionsButton } from '../Buttons/BackToQuestionsButton';
 
 export const AskPage = () => {
   const { id } = useParams();
   const params = new URLSearchParams(window.location.search);
   const entity = params.get('entity') ?? undefined;
+  let title;
+  if (id) {
+    title = 'Edit question';
+  } else if (entity) {
+    title = `Ask a question about ${formatEntityName(entity)}`;
+  } else {
+    title = 'Ask question';
+  }
+
   return (
     <Content className="qetaAskPage">
-      <ContentHeader title={id ? 'Edit question' : 'Ask question'}>
+      <ContentHeader title={title}>
         <BackToQuestionsButton />
       </ContentHeader>
       <Grid container spacing={3} direction="column">
