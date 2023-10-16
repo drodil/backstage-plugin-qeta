6d0dc62e8fe70d52302033142dfb993a832fe7ef
Heikki Hellgren <heikki.hellgren@op.fi>
2023-10-16 15:47:18 +0300

    chore: automatically update relative time

--- plugins/qeta/src/components/RelativeTimeWithTooltip/RelativeTimeWithTooltip.tsx
+++ plugins/qeta/src/components/RelativeTimeWithTooltip/RelativeTimeWithTooltip.tsx
@@ -1,14 +1,24 @@
 import { Tooltip } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect } from 'react';
 // @ts-ignore
 import RelativeTime from 'react-relative-time';
 
 export const RelativeTimeWithTooltip = (props: { value: Date | string }) => {
   const { value } = props;
   let date = value;
+  const [updates, setUpdates] = React.useState(1);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setUpdates(updates === 1 ? 0 : 1);
+    }, 30000);
+    return () => clearInterval(interval);
+  }, [updates, setUpdates]);
+
   if (typeof date === 'string' || date instanceof String) {
     date = new Date(date);
   }
+
   return (
     <Tooltip title={date.toLocaleString(navigator.languages)}>
       <RelativeTime value={date} />
