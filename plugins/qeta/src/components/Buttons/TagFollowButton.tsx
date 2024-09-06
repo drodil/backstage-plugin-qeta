32479fc0d3040cf7a01ff23909df227bc8e50bf9
Heikki Hellgren <heikki.hellgren@op.fi>
2024-09-06 12:26:42 +0300

    fix: add missing margin to follow tags button

--- plugins/qeta/src/components/Buttons/TagFollowButton.tsx
+++ plugins/qeta/src/components/Buttons/TagFollowButton.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { useTagsFollow, useTranslation } from '../../utils/hooks';
+import { useStyles, useTagsFollow, useTranslation } from '../../utils/hooks';
 import { Button, Tooltip } from '@material-ui/core';
 
 export const TagFollowButton = (props: { tag: string }) => {
   const { tag } = props;
+  const styles = useStyles();
   const { t } = useTranslation();
   const tags = useTagsFollow();
   return (
     <Tooltip title={t('tagButton.tooltip')}>
       <Button
+        className={`${styles.marginRight} qetaFollowTagBtn`}
         color={tags.isFollowingTag(tag) ? 'secondary' : 'primary'}
         onClick={() => {
           if (tags.isFollowingTag(tag)) {
