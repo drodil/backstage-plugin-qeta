7748eb38f6e669aea2b0ea5d9ad8dec6afd7cb9a
Heikki Hellgren <heikki.hellgren@op.fi>
2024-10-20 19:06:01 +0300

    fix: markdown renderer to follow theme
    closes #203

--- plugins/qeta/src/components/MarkdownRenderer/MarkdownRenderer.tsx
+++ plugins/qeta/src/components/MarkdownRenderer/MarkdownRenderer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import ReactMarkdown from 'react-markdown';
 import {
@@ -6,6 +6,66 @@ import {
   a11yLight,
 } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import { useIsDarkTheme } from '../../utils/hooks';
+import { makeStyles } from '@material-ui/core';
+import gfm from 'remark-gfm';
+
+const useStyles = makeStyles(
+  theme => ({
+    markdown: {
+      '& table': {
+        borderCollapse: 'collapse',
+        border: `1px solid ${theme.palette.border}`,
+      },
+      '& th, & td': {
+        border: `1px solid ${theme.palette.border}`,
+        padding: theme.spacing(1),
+      },
+      '& td': {
+        wordBreak: 'break-word',
+        overflow: 'hidden',
+        verticalAlign: 'middle',
+        lineHeight: '1',
+        margin: 0,
+        padding: theme.spacing(3, 2, 3, 2.5),
+        borderBottom: 0,
+      },
+      '& th': {
+        backgroundColor: theme.palette.background.paper,
+      },
+      '& tr': {
+        backgroundColor: theme.palette.background.paper,
+      },
+      '& tr:nth-child(odd)': {
+        backgroundColor: theme.palette.background.default,
+      },
+      '& a': {
+        color: theme.palette.link,
+      },
+      '& img': {
+        maxWidth: '100%',
+      },
+    },
+  }),
+  { name: 'QetaMarkdownContent' },
+);
+
+const flatten = (text: string, child: any): string => {
+  if (!child) return text;
+
+  return typeof child === 'string'
+    ? text + child
+    : React.Children.toArray(child.props.children).reduce(flatten, text);
+};
+
+const headingRenderer = (
+  props: PropsWithChildren<{ node: { tagName: string } }>,
+) => {
+  const { node, children } = props;
+  const childrenArray = React.Children.toArray(children);
+  const text = childrenArray.reduce(flatten, '');
+  const slug = text.toLocaleLowerCase('en-US').replace(/\W/g, '-');
+  return React.createElement(`${node.tagName}`, { id: slug }, children);
+};
 
 export const MarkdownRenderer = (props: {
   content: string;
@@ -13,10 +73,18 @@ export const MarkdownRenderer = (props: {
 }) => {
   const { content, className: mainClassName } = props;
   const darkTheme = useIsDarkTheme();
+  const classes = useStyles();
   return (
     <ReactMarkdown
-      className={mainClassName}
+      remarkPlugins={[gfm]}
+      className={`${classes.markdown} ${mainClassName ?? ''}`.trim()}
       components={{
+        h1: (p: any) => headingRenderer(p),
+        h2: (p: any) => headingRenderer(p),
+        h3: (p: any) => headingRenderer(p),
+        h4: (p: any) => headingRenderer(p),
+        h5: (p: any) => headingRenderer(p),
+        h6: (p: any) => headingRenderer(p),
         code(p: any) {
           const { children, className, node, ...rest } = p;
           const match = /language-(\w+)/.exec(className || '');
