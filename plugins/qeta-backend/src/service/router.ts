d40a1f92c619946b4adda02bdc50a60e765bd50a
Hellgren Heikki <heikki.hellgren@op.fi>
2025-02-03 12:33:54 +0200

    chore: refactor permissions router

--- plugins/qeta-backend/src/service/router.ts
+++ plugins/qeta-backend/src/service/router.ts
@@ -1,16 +1,6 @@
 import express from 'express';
 import Router from 'express-promise-router';
 import bodyParser from 'body-parser';
-import { createPermissionIntegrationRouter } from '@backstage/plugin-permission-node';
-import {
-  Answer,
-  ANSWER_RESOURCE_TYPE,
-  Comment,
-  COMMENT_RESOURCE_TYPE,
-  Post,
-  POST_RESOURCE_TYPE,
-  qetaPermissions,
-} from '@drodil/backstage-plugin-qeta-common';
 import { statisticRoutes } from './routes/statistics';
 import { attachmentsRoutes } from './routes/attachments';
 import { answersRoutes } from './routes/answers';
@@ -18,12 +8,12 @@ import { helperRoutes } from './routes/helpers';
 import { RouterOptions } from './types';
 import { NotificationManager } from './NotificationManager';
 import { MiddlewareFactory } from '@backstage/backend-defaults/rootHttpRouter';
-import { answerRules, commentRules, postRules } from './postRules';
 import { postsRoutes } from './routes/posts';
 import { collectionsRoutes } from './routes/collections';
 import { templateRoutes } from './routes/templates';
 import { aiRoutes } from './routes/ai';
 import { suggestionRoutes } from './routes/suggestions';
+import { permissionsRoute } from './routes/permissions';
 
 export async function createRouter(
   options: RouterOptions,
@@ -45,76 +35,12 @@ export async function createRouter(
     notificationMgr,
   };
 
-  const permissionIntegrationRouter = createPermissionIntegrationRouter({
-    permissions: qetaPermissions,
-    resources: [
-      {
-        getResources: async resourceRefs => {
-          return await Promise.all(
-            resourceRefs.map(async ref => {
-              if (!ref.startsWith('qeta:post:')) {
-                return null;
-              }
-              const id = ref.split(':')[2];
-              const post = await options.database.getPost(
-                '',
-                Number.parseInt(id, 10),
-                false,
-              );
-              return post === null ? undefined : (post as Post);
-            }),
-          );
-        },
-        resourceType: POST_RESOURCE_TYPE,
-        rules: Object.values(postRules),
-      },
-      {
-        getResources: async resourceRefs => {
-          return await Promise.all(
-            resourceRefs.map(async ref => {
-              if (!ref.startsWith('qeta:answer:')) {
-                return null;
-              }
-              const id = ref.split(':')[2];
-              const answer = await options.database.getAnswer(
-                Number.parseInt(id, 10),
-                '',
-              );
-              return answer === null ? undefined : (answer as Answer);
-            }),
-          );
-        },
-        resourceType: ANSWER_RESOURCE_TYPE,
-        rules: Object.values(answerRules),
-      },
-      {
-        getResources: async resourceRefs => {
-          return await Promise.all(
-            resourceRefs.map(async ref => {
-              if (!ref.startsWith('qeta:comment:')) {
-                return null;
-              }
-              const id = Number.parseInt(ref.split(':')[2], 10);
-              const comment =
-                (await options.database.getAnswerComment(id)) ??
-                (await options.database.getPostComment(id));
-              return comment === null ? undefined : (comment as Comment);
-            }),
-          );
-        },
-        resourceType: COMMENT_RESOURCE_TYPE,
-        rules: Object.values(commentRules),
-      },
-    ],
-  });
-
   router.get('/health', (_, response) => {
     logger.info('PONG!');
     response.json({ status: 'ok' });
   });
 
-  router.use(permissionIntegrationRouter);
-
+  permissionsRoute(router, routeOptions);
   postsRoutes(router, routeOptions);
   answersRoutes(router, routeOptions);
   helperRoutes(router, routeOptions);
---
+++ plugins/qeta-backend/src/service/routes/permissions.ts
@@ -0,0 +1,84 @@
+import { createPermissionIntegrationRouter } from '@backstage/plugin-permission-node';
+import {
+  Answer,
+  ANSWER_RESOURCE_TYPE,
+  Comment,
+  COMMENT_RESOURCE_TYPE,
+  Post,
+  POST_RESOURCE_TYPE,
+  qetaPermissions,
+} from '@drodil/backstage-plugin-qeta-common';
+import { answerRules, commentRules, postRules } from '../postRules';
+import { Router } from 'express';
+import { RouteOptions } from '../types';
+
+export const permissionsRoute = (router: Router, options: RouteOptions) => {
+  if (!options.permissions) {
+    return;
+  }
+
+  const permissions = createPermissionIntegrationRouter({
+    permissions: qetaPermissions,
+    resources: [
+      {
+        getResources: async resourceRefs => {
+          return await Promise.all(
+            resourceRefs.map(async ref => {
+              if (!ref.startsWith('qeta:post:')) {
+                return null;
+              }
+              const id = ref.split(':')[2];
+              const post = await options.database.getPost(
+                '',
+                Number.parseInt(id, 10),
+                false,
+              );
+              return post === null ? undefined : (post as Post);
+            }),
+          );
+        },
+        resourceType: POST_RESOURCE_TYPE,
+        rules: Object.values(postRules),
+      },
+      {
+        getResources: async resourceRefs => {
+          return await Promise.all(
+            resourceRefs.map(async ref => {
+              if (!ref.startsWith('qeta:answer:')) {
+                return null;
+              }
+              const id = ref.split(':')[2];
+              const answer = await options.database.getAnswer(
+                Number.parseInt(id, 10),
+                '',
+              );
+              return answer === null ? undefined : (answer as Answer);
+            }),
+          );
+        },
+        resourceType: ANSWER_RESOURCE_TYPE,
+        rules: Object.values(answerRules),
+      },
+      {
+        getResources: async resourceRefs => {
+          return await Promise.all(
+            resourceRefs.map(async ref => {
+              if (!ref.startsWith('qeta:comment:')) {
+                return null;
+              }
+              const id = Number.parseInt(ref.split(':')[2], 10);
+              const comment =
+                (await options.database.getAnswerComment(id)) ??
+                (await options.database.getPostComment(id));
+              return comment === null ? undefined : (comment as Comment);
+            }),
+          );
+        },
+        resourceType: COMMENT_RESOURCE_TYPE,
+        rules: Object.values(commentRules),
+      },
+    ],
+  });
+
+  router.use(permissions);
+};
