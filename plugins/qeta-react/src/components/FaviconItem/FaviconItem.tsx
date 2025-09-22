3557fa750f5af280b85311810b3171407d3baeac
Hellgren Heikki <heikki.hellgren@op.fi>
2025-09-22 14:39:04 +0300

    fix: use link icon if favicon fails to load

--- plugins/qeta-react/src/components/FaviconItem/FaviconItem.tsx
+++ plugins/qeta-react/src/components/FaviconItem/FaviconItem.tsx
@@ -1,11 +1,14 @@
 import { PostResponse } from '@drodil/backstage-plugin-qeta-common';
 import { qetaApiRef } from '../../api.ts';
 import { useApi } from '@backstage/core-plugin-api';
+import LinkIcon from '@material-ui/icons/Link';
+import { useState } from 'react';
 
 export const FaviconItem = (props: { entity: PostResponse }) => {
   const { entity } = props;
   const { url } = entity;
   const qetaApi = useApi(qetaApiRef);
+  const [error, setError] = useState(false);
 
   return (
     <a
@@ -18,20 +21,37 @@ export const FaviconItem = (props: { entity: PostResponse }) => {
         qetaApi.clickLink(entity.id);
       }}
     >
-      <img
-        src={`https://www.google.com/s2/favicons?domain=${encodeURIComponent(
-          url ?? '',
-        )}&sz=16`}
-        alt={url}
-        width={16}
-        height={16}
-        style={{
-          display: 'inline-block',
-          verticalAlign: 'middle',
-          objectFit: 'contain',
-          marginBottom: 5,
-        }}
-      />
+      {!error && (
+        <img
+          src={`https://www.google.com/s2/favicons?domain=${encodeURIComponent(
+            url ?? '',
+          )}&sz=16`}
+          alt={url}
+          width={16}
+          height={16}
+          onError={e => {
+            e.currentTarget.style.display = 'none';
+            setError(true);
+          }}
+          style={{
+            display: 'inline-block',
+            verticalAlign: 'middle',
+            objectFit: 'contain',
+            marginBottom: 5,
+          }}
+        />
+      )}
+      {error && (
+        <LinkIcon
+          width={16}
+          height={16}
+          style={{
+            display: 'inline-block',
+            marginBottom: 5,
+            verticalAlign: 'middle',
+          }}
+        />
+      )}
     </a>
   );
 };
