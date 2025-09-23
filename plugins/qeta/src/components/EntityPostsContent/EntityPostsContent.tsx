6dee73591c0b949ebfcdcc99adc6fff059fdd8e1
Hellgren Heikki <heikki.hellgren@op.fi>
2025-09-23 19:59:20 +0300

    fix: show users posts instead related to user in entity post content

--- plugins/qeta/src/components/EntityPostsContent/EntityPostsContent.tsx
+++ plugins/qeta/src/components/EntityPostsContent/EntityPostsContent.tsx
@@ -5,19 +5,26 @@ import {
   ViewType,
 } from '@drodil/backstage-plugin-qeta-react';
 import { Content } from '@backstage/core-components';
-import { stringifyEntityRef } from '@backstage/catalog-model';
+import { isUserEntity, stringifyEntityRef } from '@backstage/catalog-model';
 import { useEntity } from '@backstage/plugin-catalog-react';
 import { Container } from '@material-ui/core';
 
 export const EntityPostsContent = (props: PostsContainerProps) => {
   const [view, setView] = useState<ViewType>('list');
   const { entity } = useEntity();
+  const additionalProps: PostsContainerProps = {};
+  if (isUserEntity(entity)) {
+    additionalProps.author = stringifyEntityRef(entity);
+  } else {
+    additionalProps.entity = props.entity ?? stringifyEntityRef(entity);
+  }
+
   return (
     <Content>
       <Container>
         <PostsContainer
           {...props}
-          entity={props.entity ? props.entity : stringifyEntityRef(entity)}
+          {...additionalProps}
           view={view}
           onViewChange={setView}
         />
